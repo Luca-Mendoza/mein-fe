7b16c5141011cc77be65dab1789f5a35846d8d72
Luca-Mendoza-Coder <100142127+Luca-Mendoza-Coder@users.noreply.github.com>
2024-08-13 01:01:38 -0300

    fixer - stryle

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -1,7 +1,15 @@
 import { isPlatformBrowser, CommonModule } from '@angular/common';
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Inject,
+  OnInit,
+  PLATFORM_ID,
+  ViewChild,
+} from '@angular/core';
 import { ButtonArrowComponent } from '../../components/button-arrow/button-arrow.component';
 import { RouterModule } from '@angular/router';
+import { fromEvent, map } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -10,8 +18,19 @@ import { RouterModule } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+export class HomeComponent implements OnInit {
+  @ViewChild('scrollContainer', { static: true }) scrollContainer!: ElementRef;
+
+  scrollEvent$!: any;
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private elRef: ElementRef
+  ) {}
+
+  ngOnInit(): void {
+    this.scrollEventSubscribe();
+  }
 
   // Definir el enlace activo
   activeLink: string = 'About';
@@ -19,79 +38,60 @@ export class HomeComponent {
 
   // Definir los enlaces de navegación
   navLinks = [
-    { text: 'About', href: '#sectionAbout' },
-    { text: 'Experience', href: '#sectionExperience' },
-    { text: 'Projects', href: '#sectionProjects' },
+    { text: 'About', href: '#sectionAbout', id: 'sectionAbout' },
+    { text: 'Experience', href: '#sectionExperience', id: 'sectionExperience' },
+    { text: 'Projects', href: '#sectionProjects', id: 'sectionProjects' },
   ];
+
+  private scrollEventSubscribe() {
+    this.scrollEvent$ = fromEvent(
+      this.scrollContainer.nativeElement,
+      'scroll'
+    ).pipe(map(() => this.scrollContainer.nativeElement.scrollTop));
+
+    this.scrollEvent$.subscribe((scrollTop: number) => {
+      if (scrollTop < 30) {
+        this.setActiveLink({
+          text: 'About',
+          href: '#sectionAbout',
+          id: 'sectionAbout',
+        });
+      }
+      if (scrollTop > 200) {
+        this.setActiveLink({
+          text: 'Experience',
+          href: '#sectionExperience',
+          id: 'sectionExperience',
+        });
+      }
+
+      if (scrollTop > 232) {
+        this.setActiveLink({
+          text: 'Projects',
+          href: '#sectionProjects',
+          id: 'sectionProjects',
+        });
+      }
+    });
+  }
 
   // Método para establecer el enlace activo
   setActiveLink(link: any) {
     this.activeLink = link.text;
-
-    console.log({ activeLink: this.activeLink });
+    this.scrollToSection(link.id);
   }
 
-  sectionAbout() {
-    const elemento = document.getElementById('sectionAbout');
-
-    console.log({ elemento });
-
-    // if (elemento) {
-    //   elemento.scrollIntoView({ behavior: 'smooth' });
-    // }
+  private scrollToSection(sectionId: string) {
+    const elemento = document.getElementById(sectionId);
 
     if (elemento) {
       // (offset) Ajustar la posición del scroll.
-      const offset = 80;
       // Calculamos la posición del elemento con respecto a la ventana de visualización.
       // getBoundingClientRect(): Este método devuelve un objeto DOMRect que contiene información sobre el tamaño y la posición de un elemento.
-      const elementoPosicion =
-        elemento.getBoundingClientRect().top + window.scrollY;
+      const elementoPosicion = window.scrollY;
       // Realizamos un desplazamiento suave hacia la posición del elemento, ajustando la posición con el offset.
       if (isPlatformBrowser(this.platformId)) {
-        window.scrollTo({ top: elementoPosicion - offset, behavior: 'smooth' });
-      }
-    }
-  }
-
-  sectionExperience() {
-    const elemento = document.getElementById('sectionExperience');
-
-    // if (elemento) {
-    //   elemento.scrollIntoView({ behavior: 'smooth' });
-    // }
-
-    if (elemento) {
-      // (offset) Ajustar la posición del scroll.
-      const offset = 80;
-      // Calculamos la posición del elemento con respecto a la ventana de visualización.
-      // getBoundingClientRect(): Este método devuelve un objeto DOMRect que contiene información sobre el tamaño y la posición de un elemento.
-      const elementoPosicion =
-        elemento.getBoundingClientRect().top + window.scrollY;
-      // Realizamos un desplazamiento suave hacia la posición del elemento, ajustando la posición con el offset.
-      if (isPlatformBrowser(this.platformId)) {
-        window.scrollTo({ top: elementoPosicion - offset, behavior: 'smooth' });
-      }
-    }
-  }
-
-  sectionProjects() {
-    const elemento = document.getElementById('sectionProjects');
-
-    // if (elemento) {
-    //   elemento.scrollIntoView({ behavior: 'smooth' });
-    // }
-
-    if (elemento) {
-      // (offset) Ajustar la posición del scroll.
-      const offset = 80;
-      // Calculamos la posición del elemento con respecto a la ventana de visualización.
-      // getBoundingClientRect(): Este método devuelve un objeto DOMRect que contiene información sobre el tamaño y la posición de un elemento.
-      const elementoPosicion =
-        elemento.getBoundingClientRect().top + window.scrollY;
-      // Realizamos un desplazamiento suave hacia la posición del elemento, ajustando la posición con el offset.
-      if (isPlatformBrowser(this.platformId)) {
-        window.scrollTo({ top: elementoPosicion - offset, behavior: 'smooth' });
+        window.scrollTo({ top: elementoPosicion, behavior: 'smooth' });
       }
     }
   }
